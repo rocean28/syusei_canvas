e9e9bdf57f7ff1fac83eb40037fd7a65b5e73667
rWada-coder <78401386+rWada-coder@users.noreply.github.com>
2025-08-02 15:56:41 +0900

    修正

--- php/auth/users.example.php
+++ php/auth/users.example.php
@@ -3,16 +3,8 @@
 $common_password = '$2y$10$XRHw5P/voMmIpK9Xh/Q9ku/e3R6bC9U91dJkzy1kD7qO1krJ5f.pu';
 
 return [
-  'admin@syuseicanvas.co.jp' => [
-    'password' => $common_password,
-    'name' => '管理者'
-  ],
   'test@syuseicanvas.co.jp' => [
     'password' => $common_password,
     'name' => 'テスト'
-  ],
-  'test02@syuseicanvas.co.jp' => [
-    'password' => $common_password,
-    'name' => 'テスト2号機'
   ],
 ];
